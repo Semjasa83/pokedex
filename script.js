31c279eae2a083f2de39571aa30523c0fa5a1f3d
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-05-06 23:14:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+   
+let currentPokemon;
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+
+    console.log('loaded Pokemon', currentPokemon);
+
+    renderPokemonInfo();
+}
+
+function renderPokemonInfo() {
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonAvatar').src = currentPokemon ['sprites']['other']['dream_world']['front_default'];
+}
