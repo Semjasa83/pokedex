51a12876944bc80e105e1304b3fea50ed37a12bd
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-05-11 22:07:20 +0200

    for schleife

--- script copy.js
+++ script copy.js
@@ -1,21 +1,17 @@
 let allPokemon = [];
 let allPokemonArray = [];
-let allPokemonTypesArray = [];
 
 
-//fetchen und Push der API ins Array - gesamt Load 
 async function loadPokemon() {
 
     let url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`;
     let response = await fetch(url); //api fetchen und in Array allPokemon
     allPokemon = await response.json();
-    //console.log('loaded first 20 Pokemon', allPokemon); //CONSOLE
+    console.log('loaded first 20 Pokemon', allPokemon); //CONSOLE
 
     loadAllPokemonData(allPokemon);
 }
 
-
-//unterlegte URLs ins zweite Array pushen
 async function loadAllPokemonData(allPokemon) {
 
     for (let i = 0; i < allPokemon.results.length; i++) { //1te Array durchlaufen
@@ -30,33 +26,31 @@ async function loadAllPokemonData(allPokemon) {
     loadPokemonOverview();
 }
 
-
-//rendern fürs Overview und types zerpflücken
 function loadPokemonOverview() {
     document.getElementById('overviewContent').innerHTML = '';
     
     for (let i = 0; i < allPokemonArray.length; i++) {
-        //console.log(allPokemonArray[i].name);
+        console.log(allPokemonArray[i].name);
+        let allPokemonName = allPokemonArray[i].name;
+        let allPokemonAvatar = allPokemonArray[i].sprites.other.dream_world.front_default;
         document.getElementById('overviewContent').innerHTML += 
-                    templatePokemonOverview(allPokemonArray[i]);
-
-        for (let index = 0; index < allPokemonTypesArray.length; index++) {  
-            allPokemonTypesArray.push(allPokemonArray[i].types[index]); 
-        }
+                    templatePokemonOverview(
+                        allPokemonName, 
+                        allPokemonAvatar);
     }
 
 }
 
-function templatePokemonOverview(allPokemonArray) {
+function templatePokemonOverview(allPokemonName, allPokemonAvatar) {
     return /*html*/`
         <div id="overviewSingle">
                 <div id="overviewDescription">
-                    <div id="overviewName">${allPokemonArray.name}</div>
+                    <div id="overviewName">${allPokemonName}</div>
                     <div class="overview-seperate">
                         <div class="overview-type">
-                            <div id="overviewType"><!--${allPokemonArray.types[1]}--></div>
+                            <div id="overviewType">Water</div>
                         </div>
-                        <img id="overviewAvatar" src="${allPokemonArray.sprites.other.dream_world.front_default}">
+                        <img id="overviewAvatar" src="${allPokemonAvatar}">
                     </div>
                 </div>
             </div>
