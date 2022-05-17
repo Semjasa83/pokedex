31cb9241c3d5350b0b8bf62aed2fac9905b468e8
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-05-17 18:26:54 +0200

    onscroll done

--- script.js
+++ script.js
@@ -20,46 +20,44 @@ async function loadAllPokemonData(allPokemon) {
         pokemonArray = await response.json();
         allPokemonArray.push(pokemonArray); //aus 2ter URL die kompletten Daten in das 2te Array allPokemonArray
     }
-    loadOverview();
+    loadPokeIndex();
 }
 
 //rendern fürs Overview und types zerpflücken
-function loadOverview() {
+function loadPokeIndex() {
     document.getElementById('overviewContent').innerHTML = '';
     for (let i = 0; i < allPokemonArray.length; i++) {
         document.getElementById('overviewContent').innerHTML +=
-            templatePokemonOverview(allPokemonArray[i], i);
+            templatePokeIndex(allPokemonArray[i], i);
     }
 }
 
 
-function openPokemonInfo(i) {
+function openPokeDetail(i) {
     let detailValue = allPokemonArray[i];
-    let detail = document.getElementById('pokemonPopup');
-    detail.innerHTML = '';
-    detail.innerHTML += `
-    <div id="pokemonSingleBgr">
-    <div id="pokemonSingleContainer">${templateSinglePokemon(detailValue)}</div>
-    </div>`;
-    detail.classList.remove("d-none");
+    let overlay = document.getElementById('pokemonPopup');
+    let noscroll = document.getElementById('bodyScroll');
+    templateOpenPokeDetail(detailValue, overlay);
+    overlay.classList.remove("d-none");
+    noscroll.classList.add("noscrolling");
     statsCalc(detailValue);
-    return detail;
+    return overlay;
 }
 
-function closePokemonInfo() {
+function closePokeDetail() {
     let closePopup = document.getElementById('pokemonPopup');
+    let scroll = document.getElementById('bodyScroll');
     closePopup.classList.add("d-none");
+    scroll.classList.remove("noscrolling");
 }
 
 function statsCalc(detailValue) {
-    console.log(detailValue);
     let total = detailValue.stats[0].base_stat 
         + detailValue.stats[1].base_stat 
         + detailValue.stats[2].base_stat 
         + detailValue.stats[3].base_stat 
         + detailValue.stats[4].base_stat 
         + detailValue.stats[5].base_stat;
-    console.log(total);
     document.getElementById('stats_total').innerHTML = total;
 }
 
@@ -83,10 +81,17 @@ function templateTypes(pokemon) {
     return htmlCode;
 }
 
+function templateOpenPokeDetail(detailValue, overlay) {
+    overlay.innerHTML = '';
+    overlay.innerHTML += `
+    <div id="pokemonSingleBgr">
+    <div id="pokemonSingleContainer">${templatePokeDetail(detailValue)}</div>
+    </div>`;
+}
 
-function templatePokemonOverview(pokemon, i) {
+function templatePokeIndex(pokemon, i) {
     return /*html*/`
-        <div id="overview" class="cursor" onclick="openPokemonInfo(${i})">
+        <div id="overview" class="cursor" onclick="openPokeDetail(${i})">
                 <div class="overview-description">
                     <div class="overview-seperate justify">
                         <div class="overview-name">${pokemon.name}</div>
@@ -101,13 +106,13 @@ function templatePokemonOverview(pokemon, i) {
 }
 
 
-function templateSinglePokemon(detailValue) {
+function templatePokeDetail(detailValue) {
     return /*html*/`
         <div id="pokedex" class="#">
             <div class="pokedex-center">
                 <div class="align-left">
                     <div>
-                    <img class="return-button cursor" src="img/arrow-left.png" onclick="closePokemonInfo()">
+                    <img class="return-button cursor" src="img/arrow-left.png" onclick="closePokeDetail()">
                     </div>
                 </div>
                 <h1 id="pokemonName">${detailValue.name}</h1>
